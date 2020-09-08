1a3d3a0e6a62ffd0e208aac3dad55a36e2ce4951
hook278 <43997174+hook278@users.noreply.github.com>
2020-09-09 02:11:34 +0700

    Update upload_download.py

--- userbot/modules/upload_download.py
+++ userbot/modules/upload_download.py
@@ -93,7 +93,7 @@ async def download(target_file):
             c_time = time.time()
             downloaded_file_name = await target_file.client.download_media(
                 await target_file.get_reply_message(),
-                TEMP_DOWNLOAD_DIRECTORY,
+                TEMP_DOWNLOAD_DIRECTORY)
           #      progress_callback=lambda d, t: asyncio.get_event_loop(
            #     ).create_task(
             #        progress(d, t, target_file, c_time, "[DOWNLOAD]")))
